95bda172d455dcc6855d97d4bf10ba62924a852a
Marina-Golovanova <gma18@list.ru>
2021-03-14 10:32:28 +0300

    feat: add new functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (!matrix) {
+    return []
+  }
+  let newList = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 != 0) {
+      newList = newList.concat(matrix[i].reverse());
+    } else {
+      newList = newList.concat(matrix[i])
+    }
+  }
+  return newList
 }
+
